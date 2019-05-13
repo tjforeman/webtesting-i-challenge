17b769531d1e1e07a6bdd2a0b37e7e6fd3ac85f0
tjforeman <tylerforeman92@gmail.com>
2019-05-13 15:49:59 -0400

    success is passing tests

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,7 +6,11 @@ module.exports = {
 };
 
 function succeed(item) {
-  return { ...item };
+  if(item.enhancement<20){
+  return { ...item, enhancement:item.enhancement + 1};
+}else{
+  return{...item}
+}
 }
 
 function fail(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,17 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+describe('success method',()=>{
+    it('should increase enhancement by 1',()=>{
+        const item={
+            enhancement:13
+        }
+        expect(enhancer.succeed(item).enhancement).toBe(14)
+    })
+    it('should keep enhancement the same',()=>{
+        const item={
+            enhancement:20
+        }
+        expect(enhancer.succeed(item).enhancement).toBe(20)
+    })
+})
